5b9fb8352168b459ec01d0957704491b0d82de4d
unknown <muszro@hotmail.com>
2017-03-29 20:22:49 +0200

    exercise2 hw

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -1,16 +1,61 @@
 package wdsr.exercise2.procon;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.Condition;
+
 /**
  * Task: implement Buffer interface without using any *Queue classes from java.util.concurrent package.
  * Any combination of "synchronized", *Lock, *Semaphore, *Condition, *Barrier, *Latch is allowed.
  */
 public class BufferManualImpl implements Buffer {
+	Queue<Order> orders = new LinkedList<Order>();
+	Lock lock = new ReentrantLock();
+	private final Condition notFull  = lock.newCondition(); 
+	private final Condition notEmpty = lock.newCondition(); 
+	
 	public void submitOrder(Order order) throws InterruptedException {
 		// TODO
+		lock.lock();	
+		try{
+			ifFull();
+			orders.add(order);
+			notEmpty.signal();
+		} finally {
+			lock.unlock();	
+		}
+		
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+		Order tmp;
+		lock.lock();	
+		try{
+			ifEmpty();
+			tmp = orders.poll();
+			notFull.signal();
+			return tmp;
+		} finally {
+		lock.unlock();
+		}
+			
+		
 	}
+	
+		private void ifFull() throws InterruptedException {
+		  		while (orders.size() == 1200)
+		  			notFull.await();
+		   	}
+		 	
+		 	
+		 	private void ifEmpty() throws InterruptedException {
+		  		while (orders.isEmpty())
+		  			notEmpty.await();
+		   	}
 }
--- src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferQueueImpl.java
@@ -1,15 +1,18 @@
 package wdsr.exercise2.procon;
-
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 /**
  * Task: implement Buffer interface using one of *Queue classes from java.util.concurrent package.
  */
 public class BufferQueueImpl implements Buffer {
+	BlockingQueue<Order> orders = new ArrayBlockingQueue<Order>(100000);
 	public void submitOrder(Order order) throws InterruptedException {
-		// TODO
+		
+		orders.put(order);
 	}
 	
 	public Order consumeNextOrder() throws InterruptedException {
-		// TODO
-		return null;
+		
+		return orders.take();
 	}
 }
