4bac24765e5a76727786f5147e5770015603ad28
unknown <muszro@hotmail.com>
2017-03-29 20:25:00 +0200

    repair

--- src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
+++ src/main/java/wdsr/exercise2/procon/BufferManualImpl.java
@@ -11,51 +11,50 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.concurrent.locks.Condition;
 
 /**
- * Task: implement Buffer interface without using any *Queue classes from java.util.concurrent package.
- * Any combination of "synchronized", *Lock, *Semaphore, *Condition, *Barrier, *Latch is allowed.
+ * Task: implement Buffer interface without using any *Queue classes from
+ * java.util.concurrent package. Any combination of "synchronized", *Lock,
+ * *Semaphore, *Condition, *Barrier, *Latch is allowed.
  */
 public class BufferManualImpl implements Buffer {
 	Queue<Order> orders = new LinkedList<Order>();
 	Lock lock = new ReentrantLock();
-	private final Condition notFull  = lock.newCondition(); 
-	private final Condition notEmpty = lock.newCondition(); 
-	
+	private final Condition notFull = lock.newCondition();
+	private final Condition notEmpty = lock.newCondition();
+
+	private void ifFull() throws InterruptedException {
+		while (orders.size() == 1200)
+			notFull.await();
+	}
+
+	private void ifEmpty() throws InterruptedException {
+		while (orders.isEmpty())
+			notEmpty.await();
+	}
+
 	public void submitOrder(Order order) throws InterruptedException {
 		// TODO
-		lock.lock();	
-		try{
+		lock.lock();
+		try {
 			ifFull();
 			orders.add(order);
 			notEmpty.signal();
 		} finally {
-			lock.unlock();	
+			lock.unlock();
 		}
-		
+
 	}
-	
+
 	public Order consumeNextOrder() throws InterruptedException {
 		Order tmp;
-		lock.lock();	
-		try{
+		lock.lock();
+		try {
 			ifEmpty();
 			tmp = orders.poll();
 			notFull.signal();
 			return tmp;
 		} finally {
-		lock.unlock();
+			lock.unlock();
 		}
-			
-		
+
 	}
-	
-		private void ifFull() throws InterruptedException {
-		  		while (orders.size() == 1200)
-		  			notFull.await();
-		   	}
-		 	
-		 	
-		 	private void ifEmpty() throws InterruptedException {
-		  		while (orders.isEmpty())
-		  			notEmpty.await();
-		   	}
 }
